422f8b8eeb5ced527731fa4515d5bbeb9a2d8198
Zwazel <zwazel@hotmail.com>
2022-04-11 09:38:47 +0200

    hm

---
+++ tim_stuff/glassfish-test/src/main/java/dev/zwazel/glassfishtest/HelloApplication.java
@@ -0,0 +1,9 @@
+package dev.zwazel.glassfishtest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
