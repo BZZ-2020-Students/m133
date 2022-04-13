7ce65e12767df019f0efcf7d9c4b69a56ecd174e
Zwazel <zwazel@hotmail.com>
2022-04-13 21:20:34 +0200

    do a commit message and just commit everything thank you so much i love you why didnt you click the end of the tstatur i said dont not didnt you done? I-I said well done. idiot... eh ehe oh man JUST COMMIT IT

--- tim_stuff/glassfish-test/src/main/java/dev/zwazel/glassfishtest/HelloApplication.java
+++ tim_stuff/glassfish-test/src/main/java/dev/zwazel/glassfishtest/HelloApplication.java
@@ -2,7 +2,15 @@ package dev.zwazel.glassfishtest;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 @ApplicationPath("/api")
 public class HelloApplication extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        HashSet<Class<?>> classes = new HashSet<>();
+        classes.add(HelloResource.class);
+        return classes;
+    }
 }
